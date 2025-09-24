dfe1abf9b8cb5fef16c67951d51c691098bcf166
Riddhi3097 <priddhi30@gmail.com>
2025-09-24 17:48:37 -0400

    modified main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "aws-instance" "ec2-instance-1" {
+  ami = "ami-08982f1c5bf93d976"
+  instance_type= "t2.micro"
+
+  tags{
+    Name = "demo-server1"
+  }
+}
+
+resource "aws-instance" "ec2-instance-2"{
+    provider = aws.west
+    ami = "ami-00142eb1747a493d9"
+    instance_type = "t2.micro"
+
+    tags{
+        Name = "demo-server2"
+    }
+}
--- providers.tf
+++ providers.tf
@@ -2,13 +2,19 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.31.0"
+      version = ">=5.31.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
+  profile = "terraform-user"
   # Configuration options
+}
+
+provider "aws" {
+  alias = "west"
+  region = "us-west-1"
+  profile = "terrafrom-user"
 }
