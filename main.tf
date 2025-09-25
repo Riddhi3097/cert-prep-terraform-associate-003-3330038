740295f5bd94798001734824c0ecded1df72fdb2
Riddhi3097 <priddhi30@gmail.com>
2025-09-25 16:00:29 -0400

    corrected main.tf file

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2-instance-1" {
   ami = "ami-08982f1c5bf93d976"
   instance_type= "t2.micro"
 
-  tags{
+  tags = {
     Name = "demo-server1"
   }
 }
@@ -12,7 +12,7 @@ resource "aws_instance" "ec2-instance-2"{
     ami = "ami-00142eb1747a493d9"
     instance_type = "t2.micro"
 
-    tags{
+    tags = {
         Name = "demo-server2"
     }
 }
